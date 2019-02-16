16f389a967f95ca7e970006a1ea702e7daece04b
Shadi Namrouti <shadi1024@gmail.com>
2019-02-16 20:54:03 +0200

    added insertion sort

---
+++ csharp/Sort/InsertionSort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Codebase
+{
+    public static partial class Sort
+    {
+        public static void InsertionSort<T>(this T[] a, bool verbose = false) where T : struct
+        {
+            if (verbose)
+            {
+                Console.WriteLine(nameof(InsertionSort));
+                a.Print();
+            }
+
+
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                for (int j = i+1; j > 0; j--)
+                {
+
+                    if ((dynamic)a[j-1] > a[j])
+                    {
+
+                        Common.Swap(ref a[j-1], ref a[j]);
+
+                        if (verbose)
+                        {
+                            a.Print();
+                        }
+                    }
+
+                }
+
+            }
+
+
+        }
+    }
+}
