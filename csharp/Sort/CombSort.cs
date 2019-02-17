657bf5334937e34b801d629963c4874b415b9dee
Shadi Namrouti <shadi1024@gmail.com>
2019-02-17 10:28:47 +0200

    add combsort

---
+++ csharp/Sort/CombSort.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Codebase
+{
+    public static partial class Sort
+    {
+        //Based on https://en.wikipedia.org/wiki/Comb_sort#Pseudocode
+        public static void CombSort<T>(this T[] a, bool verbose = false) where T : struct
+        {
+            if (verbose)
+            {
+                Console.WriteLine(nameof(CombSort));
+                a.Print();
+            }
+
+            double gap = a.Length; // Initialize gap size
+            double shrink = 1.3; // Set the gap shrink factor
+            bool sorted = false;
+
+            while (!sorted)
+            {
+                // Update the gap value for a next comb
+                gap = Math.Floor(gap / shrink);
+                if (gap <= 1)
+                {
+                    gap = 1;
+                    sorted = true; // If there are no swaps this pass, we are done
+                }
+
+                // A single "comb" over the input list
+                int i = 0;
+
+                while (i + gap < a.Length)
+                {
+
+                    while (i + gap < a.Length) // See Shell sort for a similar idea
+                    {
+                        if ((dynamic)a[i] > a[i + (int)gap])
+                        {
+                            Common.Swap(ref a[i], ref a[i + (int)gap]);
+                            sorted = false;
+                            // If this assignment never happens within the loop,
+                            // then there have been no swaps and the list is sorted.
+
+                            if (verbose) a.Print();
+                        }
+
+                        i++;
+                    }
+
+                }
+            }
+
+        }
+    }
+}
