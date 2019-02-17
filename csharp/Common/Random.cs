ba2f46e94b53c9d8f28ac76e7de34f6252d32887
Shadi Namrouti <shadi1024@gmail.com>
2019-02-17 15:59:41 +0200

    Reviewed random functions and added BogoSort

--- csharp/Common/Random.cs
+++ csharp/Common/Random.cs
@@ -4,31 +4,58 @@ namespace Codebase
 {
     public static partial class Common
     {
-        public static T[] RandomArray<T>(int count, int min=0, int max=Int32.MaxValue, int decimalPlaces=1) 
+        public static int RandomInt(int min = 0, int max = Int32.MaxValue)
         {
-            T[] array = new T[count];
             Random rnd = new Random();
+            return (dynamic)rnd.Next(0, max);
+        }
 
-            if (typeof(T) == typeof(System.Int32))
-            {
-                for (int i = 0; i < count; i++)
-                {
-                    array[i] = (dynamic)rnd.Next(0, max);
-                }
-            }
-            else if (typeof(T) == typeof(System.Double))
-            {
-                for (int i = 0; i < count; i++)
-                {
+        public static double RandomDouble(int decimalPlaces = 1)
+        {
+            Random rnd = new Random();
+            return Math.Round(rnd.NextDouble(), decimalPlaces);
+        }
 
-                    array[i] = Math.Round((dynamic)rnd.NextDouble(),decimalPlaces);
-                }
-            }
+        public static int[] RandomArrayInt(int count, int min = 0, int max = Int32.MaxValue)
+        {
+            int[] array = new int[count];
+
+            for (int i = 0; i < count; i++)
+                array[i] = RandomInt(min, max);
 
             return array;
         }
 
-        
+        public static double[] RandomArrayDouble(int count, int decimalPlaces = 1)
+        {
+            double[] array = new double[count];
 
+            for (int i = 0; i < count; i++)
+                array[i] = RandomDouble(decimalPlaces);
+
+            return array;
+        }
+
+        public static T RandomNumber<T>(int min = 0, int max = Int32.MaxValue, int decimalPlaces = 1)
+        {
+            if (typeof(T) == typeof(System.Int32))
+                return (dynamic)RandomInt(min, max);
+            else //if (typeof(T) == typeof(System.Double))
+                return (dynamic)RandomDouble(decimalPlaces);
+        }
+
+        public static T[] RandomArray<T>(int count, int min = 0, int max = Int32.MaxValue, int decimalPlaces = 1)
+        {
+            T[] array = new T[count];
+
+            if (typeof(T) == typeof(System.Int32))
+                for (int i = 0; i < count; i++)
+                    array[i] = (dynamic)RandomInt(min, max);
+            else //if (typeof(T) == typeof(System.Double))
+                for (int i = 0; i < count; i++)
+                    array[i] = (dynamic)RandomDouble(decimalPlaces);
+
+            return array;
+        }
     }
 }
---
+++ csharp/Sort/BogoSort.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Codebase
+{
+    public static partial class Sort
+    {
+        public static void BogoSort<T>(this T[] a, bool verbose = true) where T : struct
+        {
+            if (verbose)
+            {
+                Console.WriteLine(nameof(BogoSort));
+                a.Print();
+            }
+
+            //Based on https://en.wikipedia.org/wiki/Bogosort
+
+            while (!a.IsArraySorted())
+            {
+                a.ShuffleArray();
+                if (verbose) a.Print();
+            }
+        }
+
+        private static bool IsArraySorted<T>(this T[] a)
+        {
+            bool isSorted = true;
+
+            for (int i = 0; i < a.Length - 1; i++)
+                if ((dynamic)a[i] > a[i + 1])
+                    isSorted = false;
+            return isSorted;
+
+            //Note the return can be used immediately inside the loop
+            //But this was avoided because in JQuery there is a problem doing so 
+            //i.e. if someone wants to mirror the JQuery version
+        }
+
+        //Based on https://en.wikipedia.org/wiki/Bogosort
+        //Recreates a list in random order by removing elements in random positions
+        private static T[] ShuffleArray<T>(this T[] a)
+        {
+            T[] newList = new T[a.Length];
+            int[] usedIndecies = new int[a.Length];
+
+            int index = -1;
+            int randomPosition = 0;
+
+            while (index < a.Length - 1)
+            {
+                randomPosition = Common.RandomInt(0, a.Length);
+
+                if (!IsFound(usedIndecies, index, randomPosition))
+                {
+                    index++;
+                    newList[index] = a[randomPosition];
+                    usedIndecies[index] = randomPosition;
+                }
+            }
+
+            Array.Copy(newList,a,a.Length);
+
+            return newList;
+        }
+
+        private static bool IsFound(int[] exceptionsIndices, int lastIndexToSearch, int randomPosition)
+        {
+            bool found = false;
+
+            for (int i = 0; i <= lastIndexToSearch; i++)
+                if (randomPosition == exceptionsIndices[i])
+                    found = true;
+
+            return found;
+        }
+    }
+}
