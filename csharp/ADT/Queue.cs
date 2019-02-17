b831a87353203bbf5be0d1d63217e8dfe811a629
Shadi Namrouti <shadi1024@gmail.com>
2019-02-17 18:51:08 +0200

    added stack and queue

---
+++ csharp/ADT/Queue.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Codebase
+{
+    //Based on https://en.wikipedia.org/wiki/Queue_(abstract_data_type)
+    public class QueueArray<T>
+    {
+        private T[] Items;
+        private int QFront;
+        private int QRear;
+        private readonly int MaxSize;
+
+        public QueueArray(int maxSize)
+        {
+            MaxSize = maxSize;
+            Items = new T[maxSize];
+            QFront = 0;
+            QRear = 0;
+        }
+
+        public void Enqueue(T item)
+        {
+            if (IsFull())
+                throw new Exception("Overflow!");
+            else
+                Items[QRear++] = item;
+        }
+
+        public T Dequeue()
+        {
+            if (IsEmpty())
+                throw new Exception("Underflow!");
+            else
+                return Items[QFront++];
+        }
+
+        public bool IsEmpty()
+        {
+            return QRear == QFront;
+        }
+
+        public bool IsFull()
+        {
+            return QRear == MaxSize;
+        }
+
+        public void Print()
+        {
+            Items.Print();
+        }
+
+        public int Size()
+        {
+            return QRear - QFront;
+        }
+
+        public T OnRear()
+        {
+            return Items[QRear];
+        }
+
+        public T OnFront()
+        {
+            return Items[QFront];
+        }
+
+
+    }
+}
---
+++ csharp/ADT/Stack.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Codebase
+{
+    //Based https://en.wikipedia.org/wiki/Stack_(abstract_data_type)
+    public class StackArray<T>
+    {
+        private T[] Items;
+        private int StackTop;
+        private readonly int MaxSize;
+
+        public StackArray(int maxSize)
+        {
+            MaxSize = maxSize;
+            Items = new T[maxSize];
+            StackTop = 0;
+        }
+
+        public void Push(T item)
+        {
+            if (IsFull())
+                throw new Exception("Overflow!");
+            else
+                Items[StackTop++] = item;
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty())
+                throw new Exception("Underflow!");
+            else
+                return Items[--Top];
+        }
+
+        public bool IsEmpty()
+        {
+            return StackTop == 0;
+        }
+
+        public bool IsFull()
+        {
+            return StackTop == MaxSize;
+        }
+
+        public void Print()
+        {
+            Items.Print();
+        }
+
+        public int Size()
+        {
+            return StackTop + 1;
+        }
+
+        public T OnTop()
+        {
+            return Items[StackTop];
+        }
+
+    }
+}
