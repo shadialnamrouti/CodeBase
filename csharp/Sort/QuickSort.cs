29c457f9d6e4dffbbb5120033564e55d9d42fdcb
Shadi Namrouti <shadi1024@gmail.com>
2019-02-17 16:55:31 +0200

    Added two flavors of quicksort

--- csharp/Sort/QuickSort.cs
+++ csharp/Sort/QuickSort.cs
@@ -5,6 +5,8 @@ namespace Codebase
     public static partial class Sort
     {
 
+        //Based on https://en.wikipedia.org/wiki/Quicksort
+        //Used lomuto partition scheme (see Wikipedia)
         public static void QuickSort<T>(this T[] a, bool verbose = true) where T : struct
         {
             if (verbose)
@@ -16,7 +18,6 @@ namespace Codebase
             QuickSort(a, 0, a.Length - 1, verbose);
         }
 
-        //Based on https://en.wikipedia.org/wiki/Quicksort
         public static void QuickSort<T>(this T[] a, int lo, int hi, bool verbose = true) where T : struct
         {
 
@@ -26,7 +27,7 @@ namespace Codebase
 
                 QuickSort(a, lo, pivot - 1, verbose);
                 QuickSort(a, pivot + 1, hi, verbose);
-                
+
             }
 
         }
@@ -50,6 +51,63 @@ namespace Codebase
             if (verbose) a.Print();
 
             return i;
+        }
+
+
+        //Based on https://en.wikipedia.org/wiki/Quicksort
+        //Used Hoare partition scheme (see Wikipedia)
+        public static void QuickSortV2<T>(this T[] a, bool verbose = true) where T : struct
+        {
+            if (verbose)
+            {
+                Console.WriteLine(nameof(QuickSortV2));
+                a.Print();
+            }
+
+            QuickSortV2(a, 0, a.Length - 1, verbose);
+        }
+
+        public static void QuickSortV2<T>(this T[] a, int lo, int hi, bool verbose = true) where T : struct
+        {
+
+            if (lo < hi)
+            {
+                int pivot = PartitionV2(a, lo, hi, verbose);
+
+                QuickSortV2(a, lo, pivot, verbose);
+                QuickSortV2(a, pivot + 1, hi, verbose);
+
+            }
+
+        }
+
+        //Based on https://en.wikipedia.org/wiki/Quicksort
+        private static int PartitionV2<T>(T[] a, int lo, int hi, bool verbose = true) where T : struct
+        {
+            T pivot = a[(lo + hi) / 2];
+
+            int i = lo - 1;
+            int j = hi + 1;
+
+            while (true)
+            {
+                do
+                {
+                    i++;
+                } while ((dynamic)a[i] < pivot);
+
+                do
+                {
+                    j--;
+                } while ((dynamic)a[j] > pivot);
+
+                if (i >= j)
+                    return j;
+
+                Common.Swap(ref a[i], ref a[j]);
+                if (verbose) a.Print();
+            }
+
         }
     }
 }
